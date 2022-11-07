39b3072f372ac785ca34f763f5cf29eb976464cc
lzl0125 <lianglun0125@gmail.com>
2022-11-07 18:25:14 +0800

    Add files via upload

---
+++ mainTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import junit.framework.*;
+
+class mainTest extends TestCase 
+{
+	@Test
+	public void Test() 
+	{
+		main main = new main();
+		int result_3 = main.add(2134654,465416);
+		assertEquals(result_3,2600070);
+	}
+}
