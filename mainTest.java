f0c506c87511923f665ad59eb67f8302748e7199
lzl0125 <lianglun0125@gmail.com>
2022-11-08 17:21:47 +0800

    Update mainTest.java

--- mainTest.java
+++ mainTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import junit.framework.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class mainTest extends TestCase 
 {
@@ -7,7 +8,7 @@ class mainTest extends TestCase
 	public void Test() 
 	{
 		main main = new main();
-		int result_3 = main.add(2134654,465416);
-		assertEquals(result_3,2600070);
+		int result = main.add(2134654,465416);
+		assertEquals(result,2600070);
 	}
 }
