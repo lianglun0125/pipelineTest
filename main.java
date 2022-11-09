bd48f0417d81ee21e94de40f30e6dd37269519c1
lzl0125 <lianglun0125@gmail.com>
2022-11-09 17:39:02 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,6 @@
 public class main 
-{	
-	public static void main(String[]args)
-	{
-		System.out.println(1+1);
-	}
-	
-	int add (int x, int y)
+{
+	public int add (int x, int y)
 	{
 		return x+y;
 	}
